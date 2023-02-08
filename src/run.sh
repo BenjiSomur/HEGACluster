ebbb2ae08b56a0e5ec9a5660104c7308d377434b
JuanSomur <juan.somur@gmail.com>
2023-02-08 11:00:36 -0600

    mtunis test complete

--- src/run.sh
+++ src/run.sh
@@ -1,23 +1,21 @@
 # !/bin/bash
 read -p "Enter the name of the system: " filename
-read -p "Enter the theta value: " theta
-# read -p "Enter the crossover method: " copx
-# read -p "Enter the int crossover method: " intx
-declare -a BinCrossoverArray=("cpx" "onepx")
+declare -a BinCrossoverArray=("cpx" "onepx" "uniformx" "rrx" "mx")
 declare -a IntCrossoverArray=("px1" "exchange")
 
 eval "$(conda shell.bash hook)"
+source ~/anaconda3/etc/profile.d/conda.sh
 conda activate general
 
 for val in ${BinCrossoverArray[@]}; do
     for ix in ${IntCrossoverArray[@]}; do
         for ((i = 1; i<=31; i++)) do
-            python3 main.py $i $filename $theta $val $ix
+            python3 main.py $i $filename $val $ix
             echo "Execution $i of $val and $ix finished"
         done
         echo "Evaluation of int crossover $ix Finished"
     done
-    echo "Evaluation of bin crossover $val Finished"
+    echo "Evaluation of $val Finished"
 done
 
 echo "Computation complete :)"
